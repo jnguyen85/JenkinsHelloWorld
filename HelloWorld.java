8fb207dc7d68595660f66796b9e241649429ca57
jnguyen8585 <nguyen8585@gmail.com>
2021-11-17 22:50:08 -0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+ public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
+
+
+// dfsdflds39393llkdkkkdkkd
+
+// JENKINS_URL/job/HelloWorld/build?token=TOKEN_NAME
